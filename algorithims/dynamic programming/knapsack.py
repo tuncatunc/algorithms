904175e347d4f7574ddb8c625fb4e491aad251d8
Tunca Tunc <atuncatunc@gmail.com>
2019-09-10 21:06:58 +0300

    Knapsacak memoized solution

--- algorithims/dynamic programming/knapsack.py
+++ algorithims/dynamic programming/knapsack.py
@@ -16,6 +16,9 @@ class KnapsackProblem:
 
     self.solution = set([])
 
+    # Memo for the memoization solution
+    self.memo = [[None for n in range(C + 1)] for w in range(N)]
+
   # n: Item index
   # C: Remaining capacity
   def solve(self, n, c):
@@ -38,10 +41,45 @@ class KnapsackProblem:
     result = max(included, notIncluded)
     return result
 
+  # n: Item index
+  # C: Remaining capacity
+  def solveMemo(self, n, c):
+
+    # Base case
+    if n < 0 or c <= 0:
+      return 0
+    
+    if self.W[n] > c:
+      return 0
+
+    # Return memoized solution if any
+    if self.memo[n][c] != None:
+      return self.memo[n][c]
+
+    included = self.solveMemo(n-1, c - self.W[n]) + self.V[n]
+    notIncluded = self.solveMemo(n - 1, c)
+
+    if included > notIncluded:
+      self.solution.add(n)
+
+    result = max(included, notIncluded)
+    self.memo[n][c] = result
+
+    return result
+
   def printSolution(self):
+    print("=============================")
     print(f"Max knapsack value {self.max}")
     print(f"Items {self.solution}")
+    print(f"Weights {self.W}")
+    print(f"Values {self.V}")
 
+
+  # Memoized solution
+  def solveProblemMemoized(self):
+    self.max = self.solveMemo(numOfItems - 1, capacityOfKnapsack)
+
+  # Recursive solution
   def solveProblem(self):
     self.max = self.solve(numOfItems - 1, capacityOfKnapsack)
     
@@ -54,4 +92,7 @@ weights = [1, 2, 4, 2, 5]
 knapsack = KnapsackProblem(
     weights, values, capacityOfKnapsack, numOfItems)
 knapsack.solveProblem()
+knapsack.printSolution()
+
+knapsack.solveProblemMemoized()
 knapsack.printSolution()
